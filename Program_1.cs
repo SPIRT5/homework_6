28241c93d3670bff22363262ce1ffe2a13d4722f
SPIRT5 <108623559+SPIRT5@users.noreply.github.com>
2022-07-13 20:41:34 +0300

    Homework_6
    Homework_6

---
+++ Program_1.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+int GetNumber (string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число, введите число!";
+
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out result)) 
+        {
+            break;
+        }
+        else 
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+
+int length = 8;
+int[] numberArray = new int[length];
+int result = 0;
+
+for (int i = 0; i < length; i++)
+{
+    numberArray[i] = GetNumber("Введите число: ");
+    if (numberArray[i] > 0) 
+    {
+        result++;
+    }
+}
+
+for (int i = 0; i < length; i++)
+{
+    Console.Write(numberArray[i] + ", ");
+}
+
+Console.WriteLine();
+Console.WriteLine("Кол-во чисел больше 0 в массиве: " + result);
---
+++ Program_2.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+int GetNumber (string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число, введите число!";
+
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out result)) 
+        {
+            break;
+        }
+        else 
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+
+double b1 = GetNumber("Введите координату b1: "); //2
+double k1 = GetNumber("Введите координату k1: "); //5
+double b2 = GetNumber("Введите координату b2: "); //4
+double k2 = GetNumber("Введите координату k2: "); //9
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine("Точка пересечения двух прямых: " + x + "; " + y);
+
+
